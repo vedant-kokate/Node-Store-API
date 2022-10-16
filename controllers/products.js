69fa10ba8a0e7fc6b12db9b479aa3a033c59b000
vedantkokate07 <vedant.kokate07@gmail.com>
2022-10-16 13:38:03 +0530

    initial search query

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
-const Products = require("../models/product")
+const Product = require("../models/product")
 
 const getAllProductsStatic = async (req,res)=>{
-   const product = await Products.find({})
+   const product = await Product.find({featured:true})
     res.status(200).json({msg:product})
 }
 
 const getAllProducts = async (req,res)=>{
-    res.status(200).json({msg:'products route'})
+    const products = await Product.find(req.query)
+    res.status(200).json({msg:products,nbhits:products.length})
 }
 
 module.exports={
