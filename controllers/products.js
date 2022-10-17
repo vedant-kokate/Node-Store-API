37ced44f5da77641d8a43438c9c1442a68d5edf3
vedantkokate07 <vedant.kokate07@gmail.com>
2022-10-17 11:02:08 +0530

    numericFilters

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req,res)=>{
 }
 
 const getAllProducts = async (req,res)=>{
-    const {featured, company,name, sort} = req.query
+    const {featured, company,name, sort,fields, numericFilters} = req.query
     const queryObj = {}
     if (featured){
         queryObj.featured = featured==='true'?true:false
@@ -18,16 +18,44 @@ const getAllProducts = async (req,res)=>{
     if (name){
         queryObj.name = {$regex:name  ,$options:'i'}
     }
+    if(numericFilters){
+        const operationMap={
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<=':'$lte',
+        }
+        const regEx = /\b(>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx,(match)=>`-${operationMap[match]}-`);
+        const options = ['price','rating']
+        filters.split(",").forEach(element => {
+            const [field,operator,value] = element.split("-")
+            if (options.includes(field)){
+                queryObj[field]={[operator]:Number(value)}
+            }
+            
+        });
+    }
     console.log(queryObj);
     let result = Product.find(queryObj)
     if (sort){
         let sortList = sort.replace(","," ")
-        console.log(sortList)
+        
         result = result.sort(sortList)
     }
     else{
         result = result.sort('createdAt')
     }
+    if (fields){
+        const fieldList = fields.replace(","," ")
+        console.log(fieldList)
+        result = result.select(fieldList )
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page-1)*limit;
+    result = result.skip(skip).limit(limit);
     const products = await result
 
 
